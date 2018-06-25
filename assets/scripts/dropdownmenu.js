f33eed0ae37fe392b49f73d348d6545f474062ad
bienfromage <100codemaster@gmail.com>
2018-06-25 11:40:53 -0400

    website revamped

---
+++ assets/scripts/dropdownmenu.js
@@ -0,0 +1,39 @@
+//scripts for dropdown menu
+//use in conjunction with assets/styles/styles.css
+
+"use strict";
+
+//onclick restyle dropdown link, show child dropdown backgroudn and grandchildren links
+function openMenu(dropdownToOpen){
+  dropdownToOpen.classList.toggle("selected");
+  let child = dropdownToOpen.children[0];
+  child.classList.toggle("show");
+  let grandChildren = child.children;
+  for(let i = 0; i<grandChildren.length;i++){
+    grandChildren[i].classList.toggle("show");
+  }
+}
+
+//user clicks off in space close the dropdown menu
+window.onclick = function(event) {
+  if (!event.target.matches('.menu-item-dropdown')) {
+    let allDropdownLinks = document.getElementsByClassName('dropdown-link');
+    for(let i = 0; i<allDropdownLinks.length; i++){
+      if(allDropdownLinks[i].classList.contains("show"))
+        allDropdownLinks[i].classList.remove("show");
+    }
+    
+    let allDropdowns = document.getElementsByClassName('dropdown');
+    for(let i = 0; i<allDropdowns.length; i++){
+      if(allDropdowns[i].classList.contains("show"))
+        allDropdowns[i].classList.remove("show");
+    }
+    
+    
+    let allMenuItemDropdowns = document.getElementsByClassName('menu-item-dropdown');
+    for(let i = 0; i<allMenuItemDropdowns.length; i++){
+      if(allMenuItemDropdowns[i].classList.contains("selected"))
+        allMenuItemDropdowns[i].classList.remove("selected");
+    }
+  }
+};
---
+++ assets/scripts/loadProjects.js
@@ -0,0 +1,50 @@
+//this file uses jQuery 3.3.1
+//generate page data from project .json file
+
+"use strict";
+
+function setup(year){
+  if(year){
+    //TODO:change to local file before upload
+    year=year[1];
+    $.getJSON("http://cheesetech.fun/project-data.json",(jsonString)=>{
+      try{
+        $('.project-list').append("<h1>"+year+"</h1>");
+        for(let i=0; i<jsonString[year].length;i++){
+          let project = jsonString[year][i];
+          
+            $('.project-list').append("<a href='"+project.link+"'><div class='project'><div class='project-info'><h2>"+project.title+"</h2><p>"+project.description+"</p></div></div></a>");
+        }
+      }catch(e){
+        console.error("Problem parsing JSON: "+e);
+        setup(null);
+      }
+    });
+  }else{
+    $.getJSON("http://cheesetech.fun/project-data.json",(jsonString)=>{
+      try{
+        let errorMessage="<h1 style='text-align:center'>I'm afraid I can't do that dave</h1><div style='text-align:center;color:black;'><img src='assets/images/32px-HAL9000_Better_Reflection.png' alt='HAL 9000'/><p>We couldn't find any projects of that description. Try using one of the links below to navigate through projects by year.</p>";
+        for(let key in jsonString){
+          errorMessage+="<a href='https://bienfromage.github.io/projects?year="+key+"' class='error-link'>"+key+"</a>";
+        }
+        
+        $('.project-list').html(errorMessage);
+      }catch(e){
+        console.error("Error getting JSON: "+e);
+        $('.project-list').html("<h1>I'm afraid I can't do that dave</h1><div style='text-align:center;color:black;'><img src='assets/images/32px-HAL9000_Better_Reflection.png' alt='HAL 9000'/><p>We couldn't find any projects of that description.");
+      }
+    });
+  }
+}
+
+//make spacing more friendly to browser by determining height with js and adjusting main div accordingly.
+function resize(){
+  $(".main").css("margin-top",$(".menu").css("height"));
+}
+
+//get year from querystring and display projects
+setup((new RegExp( '[?&]year=([^&#]*)', 'i' )).exec(window.location.href));
+
+resize();
+//move screen to accomadate changes in size
+window.addEventListener('resize',resize);
